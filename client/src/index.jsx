da193dd1b3b937ea6a814e4003c1a6b507509615
Kym Hooper <khoupp@gmail.com>
2020-09-23 19:11:35 -0600

    React, webpack, babel, config, bootstrap, airbnb linter, jest, enzyme

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+// eslint-disable-next-line import/extensions
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
