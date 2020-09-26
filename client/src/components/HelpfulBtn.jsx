5f0f3d003e00082563ed88051e661cd9f7a745af
Kym Hooper <khoupp@gmail.com>
2020-09-26 12:45:54 -0600

    Organize HelpfulBtn tags and use flex box

--- client/src/components/HelpfulBtn.jsx
+++ client/src/components/HelpfulBtn.jsx
@@ -1,32 +1,30 @@
 import React, { useState } from 'react';
-import { Button, Row, Col, Container } from 'react-bootstrap';
+import { Alert, Button, Row, Col, Container } from 'react-bootstrap';
 
-const Helpful = (props) => {
+const HelpfulBtn = (props) => {
   // Declare a new state variable, called "count"
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <Row>
-        <span>
-          Helpful?
-         </span>
-         <Button
-          variant="dark"
-          size="sm"
-          onClick={() => setCount(count + 1)}
-         >
-          Yes{`(${count})`}
-         </Button>
-         <Button
-          variant="dark"
-          size="sm"
-         //  onClick={() => setCount(count + 1)}
-         >Add Answer
-         </Button>
-      </Row>
-    </div>
+
+      <span>Helpful? {' '}
+      <Alert.Link
+        className="helpful-click"
+        variant="dark"
+        size="sm"
+        onClick={() => setCount(count + 1)}
+      >
+      Yes{`(${count}) `}
+      </Alert.Link>
+      <Alert.Link
+        variant="dark"
+        size="sm"
+      //  onClick={() => setCount(count + 1)}
+      >Add Answer
+      </Alert.Link>
+      </span>
+
   );
 }
 
-export default Helpful;
+export default HelpfulBtn;
