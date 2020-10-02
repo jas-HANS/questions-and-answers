2fad1ec3b2a9cfbeb557065b607ed172150980f3
Kym Hooper <khoupp@gmail.com>
2020-10-02 14:53:28 -0600

    Finally! Fix helpful button css

--- client/src/components/HelpfulBtn.jsx
+++ client/src/components/HelpfulBtn.jsx
@@ -5,33 +5,30 @@ const HelpfulBtn = ({clickFunc, id, currentCount}) => {
   const [count, setCount] = useState(currentCount);
   const [disabled, setDisabled] = useState(false);
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    clickFunc(id);
     setCount(count + 1);
     setDisabled(!disabled);
   };
 
-  // STRETCH GOAL, one click votes helpful,
-  // additional click removes helpful
-  // const handleXtraClick = () => {
-  //   setCount(count - 1);
-  //   setDisabled(!disabled);
-  // };
-
   return (
     <span>Helpful? {' '}
-      <Alert.Link
+      <a
         className="helpful-click"
         disabled={disabled}
-        onClick={(e) => {
-          e.preventDefault();
-          clickFunc(id);
-          handleClick();
-          // handleXtraClick();
-        }}
+        onClick={!disabled ? handleClick : null}
       >Yes{`(${count}) `}
-      </Alert.Link>
+      </a>
     </span>
   );
 };
 
 export default HelpfulBtn;
+
+// STRETCH GOAL, one click votes helpful,
+// additional click removes helpful
+// const handleXtraClick = () => {
+//   setCount(count - 1);
+//   setDisabled(!disabled);
+// };
