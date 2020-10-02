56b9e324bbeab7321c94397f7a5bb10c225555c6
Kym Hooper <khoupp@gmail.com>
2020-10-01 21:21:49 -0600

    Refactored/made subcomponents for button/modal/form

--- client/src/components/SubmitQModal.jsx
+++ client/src/components/SubmitQModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Modal, Button, Form} from 'react-bootstrap';
+import SubmitQForm from './SubmitQForm.jsx';
 
 const SubmitQModal = ({show, question, onHide}) => {
   return (
@@ -21,27 +22,8 @@ const SubmitQModal = ({show, question, onHide}) => {
           <Modal.Body>
            About the [product name - via product detail get req?]
             <br></br>
-            {/* form goes here */}
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                Well never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-              Submit
-              </Button>
-            </Form>
+            <SubmitQForm/>
 
           </Modal.Body>
         </Modal.Dialog>
