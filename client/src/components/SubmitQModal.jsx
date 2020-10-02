59b43e5763556745cdc32abfc6a3ab17c82518cb
Kym Hooper <khoupp@gmail.com>
2020-10-02 12:25:55 -0600

    Adjust modal title, subtitle, and styling

--- client/src/components/SubmitQModal.jsx
+++ client/src/components/SubmitQModal.jsx
@@ -5,28 +5,22 @@ import SubmitQForm from './SubmitQForm.jsx';
 const SubmitQModal = ({show, question, onHide}) => {
   return (
     <div>
-      <Modal
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
+      <Modal closeButton
+        // aria-labelledby="contained-modal-title-vcenter"
         centered
         show={show}
         onHide={onHide}
       >
-        <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
              Ask Your Question
-            </Modal.Title>
-          </Modal.Header>
+          </Modal.Title>
+        </Modal.Header>
 
-          <Modal.Body>
-           About the [product name - via product detail get req?]
-            <br></br>
-
-            <SubmitQForm/>
-
-          </Modal.Body>
-        </Modal.Dialog>
+        <Modal.Body>
+        ...about the [product name - via product detail get req?]
+          <SubmitQForm/>
+        </Modal.Body>
       </Modal>
     </div>
   );
