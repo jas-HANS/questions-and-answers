5d1109385737da539078ccc9015e6daaf03be621
Kym Hooper <khoupp@gmail.com>
2020-10-01 20:45:22 -0600

    Created component, linked click functionality, imported dummy modal

---
+++ client/src/components/SubmitButton.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {Button} from 'react-bootstrap';
+import SubmitQModal from './SubmitQModal.jsx';
+
+const SubmitButton = ({question}) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+  return (
+    <div>
+      <Button id="addq-btn"variant="secondary" size="sm"
+        onClick={handleShow}
+      >ADD A QUESTION
+      </Button>
+
+      <SubmitQModal
+        show={show}
+        question={question}
+        onHide={handleClose}
+      />
+    </div>
+  );
+};
+
+export default SubmitButton;
