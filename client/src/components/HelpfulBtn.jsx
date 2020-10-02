296fd49a8b65d4eb47c03c7b81dde9aeb39784c2
Kym Hooper <khoupp@gmail.com>
2020-10-02 17:05:37 -0600

    Able to toggle off helpfulness clickability

--- client/src/components/HelpfulBtn.jsx
+++ client/src/components/HelpfulBtn.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import Alert from 'react-bootstrap/Alert';
 
 const HelpfulBtn = ({clickFunc, id, currentCount}) => {
   const [count, setCount] = useState(currentCount);
