6aafecdd5a65e1a970baf8362623b9b52a46db5e
Kym Hooper <khoupp@gmail.com>
2020-10-02 20:04:26 -0600

    Code clean up

--- client/src/components/QuestionList.jsx
+++ client/src/components/QuestionList.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable max-len */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Question from './Question';
 import SubmitButton from './SubmitButton.jsx';
 
 // eslint-disable-next-line react/prop-types
-const QuestionList = ({qList, isHelpfulQ, isHelpfulA}) => {
-  const mappedList = qList.map((quest, i) => <Question
-    question={quest}
+const QuestionList = ({qList, isHelpfulQ, isHelpfulA, getProductQs, productName}) => {
+  const [load, setLoad] = useState(true);
+  // const [productName, setProductName] = useState('');
+
+  const mappedList = qList.map((question, i) => <Question
+    question={question}
+    getProductQs={getProductQs}
     key={i}
     isHelpfulQ={isHelpfulQ}
     isHelpfulA={isHelpfulA}
+    productName={productName}
   />);
-
-  const [load, setLoad] = useState(true);
 
   return (
     <div>
@@ -46,7 +50,10 @@ const QuestionList = ({qList, isHelpfulQ, isHelpfulA}) => {
         {mappedList.length > 0 &&
       <><br></br>
         <div>
-          <SubmitButton/>
+          <SubmitButton
+            // qList={qList}
+            productName= {productName}
+          />
         </div>
       </>}
       </div>
