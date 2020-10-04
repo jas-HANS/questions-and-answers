8d3de9ee5ff0d36ed4f13e1d258bff473ad09932
Kym Hooper <khoupp@gmail.com>
2020-10-03 18:16:22 -0600

    Refactored. Submit q button works, but only up to 5 qs

--- client/src/components/QuestionList.jsx
+++ client/src/components/QuestionList.jsx
@@ -17,49 +17,78 @@ const QuestionList = ({qList, isHelpfulQ, isHelpfulA, productName, getId, getPro
     isHelpfulQ={isHelpfulQ}
     isHelpfulA={isHelpfulA}
     getId={getId}
-    getProductQs={getProductQs}
+    // getProductQs={getProductQs}
   />);
 
   return (
     <div>
       <div className="question-list">
         {mappedList.slice(0, 4)}
-        {mappedList.length > 4 &&
-        <Accordion>
-          <Accordion.Collapse
-            eventKey="0">
-            <div>
-              {mappedList.slice(4)}
-            </div>
-          </Accordion.Collapse>
-          <Accordion.Toggle
-            as={Card.Header}
-            style={{margin: '10px'}}
-            variant="link"
-            eventKey="0"
-            onClick={() => setLoad(!load)}
-          >
-            {load && 'LOAD MORE QUESTIONS'}{!load && 'COLLAPSE QUESTIONS'}
-          </Accordion.Toggle>
-        </Accordion>}
+        {mappedList.length > 3 &&
+      <Accordion>
+        <Accordion.Collapse
+          eventKey="0">
+          <div>
+            {mappedList.slice(4)}
+          </div>
+        </Accordion.Collapse>
+        <Accordion.Toggle
+          as={Card.Header}
+          style={{margin: '10px'}}
+          variant="link"
+          eventKey="0"
+          onClick={() => setLoad(!load)}
+        >
+          {load && 'LOAD MORE QUESTIONS'}{!load && 'COLLAPSE QUESTIONS'}
+        </Accordion.Toggle>
+      </Accordion>}
 
         {mappedList.length < 1 &&
-        <SubmitButton className="addq-btn-solo"/>}
+      <SubmitButton className="addq-btn-solo"
+        productName={productName}
+        getId={getId}
+        getProductQs={getProductQs}/>}
       </div>
       <div>
         {mappedList.length > 0 &&
-      <><br></br>
-        <div>
-          <SubmitButton
-            productName={productName}
-            getId={getId}
-            getProductQs={getProductQs}
-          />
-        </div>
-      </>}
+    <><br></br>
+      <div>
+        <SubmitButton
+          productName={productName}
+          getId={getId}
+          getProductQs={getProductQs}
+        />
+      </div>
+    </>}
       </div>
     </div>
   );
 };
 
 export default QuestionList;
+
+// return (
+//   <div>
+//     <div className="question-list">
+//     {mappedList.length > 4 ?
+//     mappedList.slice(0, 4)
+//     <Accordion>
+//       <Accordion.Collapse
+//         eventKey="0">
+//         <div>
+//           {mappedList.slice(4)}
+//         </div>
+//       </Accordion.Collapse>
+//       <Accordion.Toggle
+//         as={Card.Header}
+//         style={{margin: '10px'}}
+//         variant="link"
+//         eventKey="0"
+//         onClick={() => setLoad(!load)}
+//       >
+//         {load && 'LOAD MORE QUESTIONS'}{!load && 'COLLAPSE QUESTIONS'}
+//       </Accordion.Toggle>
+//     </Accordion>
+//     : mappedList}
+
+
