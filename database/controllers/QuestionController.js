73d513122fa0ea2f8fffc7481ea3d6b6270e02a8
Zak DeBrine <zdebrine@gmail.com>
2020-10-14 19:54:43 -0600

    Built out AnswerModel design

--- database/controllers/QuestionController.js
+++ database/controllers/QuestionController.js
@@ -2,7 +2,7 @@ const { Questions } = require('../models/QuestionModel.js');
 
 const QuestionController = {
     getAllAnswers: (question, callback) => {
-        Questions.find({product_id: question.question_id}, (err, data) => {
+        Questions.find({question_id: question.question_id}, (err, data) => {
             if (err) {
                 callback(err, null)
             } else {
--- database/models/AnswerModel.js
+++ database/models/AnswerModel.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const answerSchema = new Schema({
+    answer_id: Number,
+    body: String,
+    date: Date,
+    answerer_name: String,
+    helpfulness: Number,
+    photos: [{
+        id: Number,
+        url: String
+    }],
 });
 
-const Answers = mongoose.model("Answer", answerSchema);
+const Answers = mongoose.model("Answers", answerSchema);
 
 
 module.exports = {
